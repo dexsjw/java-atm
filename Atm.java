a3d3d5d879e9047a4356a7f7c8d8d3e9fc0e2a06
Dexter <junweidexter.soh@dxc.com>
2024-08-05 17:51:22 +0800

    completed atm functionality

--- Atm.java
+++ Atm.java
@@ -5,13 +5,16 @@ public class Atm {
         double balance = 0.0;
         boolean isRunning = true;
 
-        System.out.println("=====================");
-        System.out.println("=     Dex's ATM     =");
-        System.out.println("=====================");
+        System.out.println("====================");
+        System.out.println("=     Java ATM     =");
+        System.out.println("====================");
 
         String userPin = System.console().readLine("Please enter pin: ");
+        String welcomeMessage = userPin.equals(PIN)
+            ? "Welcome! Please choose one of the options below by entering the respective option number."
+            : "You have entered an incorrect pin. Please try again, goodbye!";
+        System.out.println(welcomeMessage);
         if (!userPin.equals(PIN)) {
-            System.out.println("You have entered an incorrect pin. Please try again, goodbye!");
             return;
         }
 
@@ -19,40 +22,49 @@ public class Atm {
             System.out.println("1. Check Balance");
             System.out.println("2. Deposit Money");
             System.out.println("3. Withdraw Money");
-            System.out.println("4. Exit");
+            System.out.println("4. Exit\n");
             
             String userInput = System.console().readLine("Option: ");
     
             switch (userInput) {
                 case "1": {
-                    System.out.println("Check Balance");
-                    System.out.println("=====================");
-                    System.out.println("You have " + balance + " in your balance");
+                    System.out.println("== Check Balance ===");
+                    System.out.println("====================");
+                    System.out.println("You have " + balance + " in your balance\n");
                     break;
                 }
                 case "2": {
-                    System.out.println("Deposit Money");
-                    System.out.println("=====================");
+                    System.out.println("== Deposit Money ===");
+                    System.out.println("====================");
                     double amountToDeposit = Double.parseDouble(System.console().readLine("Enter amount to deposit: "));
+                    // TODO: check if user input numbers
                     balance += amountToDeposit;
-                    System.out.println("Deposit successful! You now have " + balance + " in your balance");
+                    System.out.println("Deposit successful! You now have " + balance + " in your balance\n");
                     break;
                 }
                 case "3": {
-                    System.out.println("Withdraw Money");
-                    System.out.println("=====================");
+                    System.out.println("== Withdraw Money ==");
+                    System.out.println("====================");
                     double amountToWithdraw = Double.parseDouble(System.console().readLine("Enter amount to withdraw: "));
-                    String message = amountToWithdraw > balance ? "Withdraw successful!" : "Withdraw unsuccessful! You have insufficient funds in your balance";
-    
+                    // TODO: check if user input numbers
+                    String message;
+                    if (amountToWithdraw <= balance) {
+                        balance -= amountToWithdraw;
+                        message = "Withdraw successful! You now have " + balance + " in your balance";
+                    } else {
+                        message = "Withdraw unsuccessful! You have insufficient funds in your balance for withdrawl of " + amountToWithdraw
+                            + "\nCurrent balance: " + balance;
+                    }
+                    System.out.println(message + "\n");
                     break;
                 }
                 case "4": {
-                    System.out.println("Exiting App");
+                    System.out.println("=== Exiting App ====");
                     isRunning = false;
                     break;
                 }
                 default: {
-                    System.out.println("Invalid Input! Please enter a number between 1 to 4.");
+                    System.out.println("Invalid Input! Please enter a number between 1 to 4.\n");
                 }
             }
         } while (isRunning);
